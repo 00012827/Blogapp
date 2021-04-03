9b993838b136683af83ed84c9a060ea14558d444
Akrom <odilovakrom25@gmail.com>
2021-04-04 00:32:46 +0500

    styles and home pages were added

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(5000, err => {
+  if (err) console.log(err)
+
+  console.log('Server is running on port 5000...')
+})
